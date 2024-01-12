2cac3f2ed6a20fe8c50f7f16ad164922d7d9968f
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-01-12 16:50:23 +0300

    refactor: move rules from phi-paper

--- eo-phi-normalizer/src/Language/EO/Phi/Rules/Common.hs
+++ eo-phi-normalizer/src/Language/EO/Phi/Rules/Common.hs
@@ -78,20 +78,6 @@ applyRulesChain ctx obj
       | obj' <- applyOneRule ctx obj
       , chain <- applyRulesChain ctx obj' ]
 
--- * Yegor's Rules
-
--- | Rule 6.
-rule6 :: Rule
-rule6 ctx (ObjectDispatch (Formation bindings) a)
-  | Just obj <- lookupBinding a bindings = do
-    guard (isNF ctx obj)
-    return (Application obj [AlphaBinding Rho (Formation bindings')])
-    where
-      bindings' = filter (not . isDispatched) bindings
-      isDispatched (AlphaBinding a' _) = a == a'
-      isDispatched _ = False
-rule6 _ _ = Nothing
-
 -- * Helpers
 
 -- | Lookup a binding by the attribute name.
---
+++ eo-phi-normalizer/src/Language/EO/Phi/Rules/PhiPaper.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Language.EO.Phi.Rules.PhiPaper where
+
+import Control.Monad (guard)
+import Language.EO.Phi
+import Language.EO.Phi.Rules.Common
+
+-- * Yegor's Rules
+
+-- | Rule 1.
+rule1 :: Rule
+rule1 _ = \case
+    Formation bindings ->
+        let Program bindings' = normalize (Program bindings)
+         in Just (Formation bindings')
+    _ -> Nothing
+
+-- | Rule 6.
+rule6 :: Rule
+rule6 ctx (ObjectDispatch (Formation bindings) a)
+    | Just obj <- lookupBinding a bindings = do
+        guard (isNF ctx obj)
+        return (Application obj [AlphaBinding Rho (Formation bindings')])
+  where
+    bindings' = filter (not . isDispatched) bindings
+    isDispatched (AlphaBinding a' _) = a == a'
+    isDispatched _ = False
+rule6 _ _ = Nothing
