1481ad704b5a37ceff88e4a316438de7204bc4da
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-01-16 17:01:48 +0300

    feat: run user-defined tests
    Co-authored-by: Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>

---
+++ eo-phi-normalizer/test/Test/EO/Yaml.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.EO.Yaml where
+
+import Control.Monad (forM)
+import Data.List (sort)
+import Language.EO.Phi.Rules.Yaml (RuleSet, parseRuleSetFromFile)
+import System.Directory (listDirectory)
+import System.FilePath ((</>))
+
+fileTests :: FilePath -> IO RuleSet
+fileTests = parseRuleSetFromFile
+
+directoryTests :: FilePath -> IO [RuleSet]
+directoryTests dir = do
+    paths <- listDirectory dir
+    forM (sort paths) $ \path ->
+        fileTests (dir </> path)
