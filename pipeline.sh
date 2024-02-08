e958b4829b05bd2cc42f524023a637bfc757e226
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-02-08 08:54:08 +0300

    refactor: pipeline script

--- pipeline.sh
+++ pipeline.sh
@@ -1,103 +1,115 @@
 set -euo pipefail
 
-ROOT_DIR="$PWD"
-
-mkdir -p ".pipeline/$PROGRAM"
-cd ".pipeline/$PROGRAM"
+stack run transform-eo-tests
 
 shopt -s expand_aliases
-
-EO="0.34.3"
+EO="0.34.1"
 alias eo="eoc --parser=${EO}"
 
-cp "$ROOT_DIR"/pipeline/programs/"$PROGRAM"/app.eo .
+cd pipeline/eo
+eoc clean
+eoc phi
+cd ..
 
-# Without normalizer
+mkdir -p phi
+cp eo/.eoc/phi/*.phi phi
 
-eo clean
-eo link
-eo --alone dataize app > before.txt
-cp before.txt "$ROOT_DIR"/pipeline/programs/"$PROGRAM"/before.txt
+cd phi
+# TODO #100:15min run normalizer
+# normalizer should create phi-normalized
+cd ..
 
-# With normalizer
+# TODO #100:15min run unphi
+# cd phi-normalized
+# eo unphi
+# cd ..
+# mkdir -p eo-normalized
+# copy normalized files to eo-normalized
 
-eo phi
-
-IO=".eoc/phi/app.phi"
-I=".eoc/phi/app.bk.phi"
-mv "$IO" "$I"
-
-stack run normalize-phi < "$I" > "$IO" \
-  || {
-    cat <<EOF
-Normalizer failed!
-
-
-* EO expression:
-
-$(cat app.eo)
+# TODO #100:15min run tests
+# cd eo-normalized
+# eo test
 
 
-* Phi expression:
 
-$(cat "$I")
+# TODO #100:15min remove old code
+# eo phi
 
+# IO=".eoc/phi/app.phi"
+# I=".eoc/phi/app.bk.phi"
+# mv "$IO" "$I"
 
-* Error:
-
-$(cat "$IO")
-EOF
-    mv "$I" "$IO"
-    exit 1
-  }
-
-{
-  export LC_ALL="C"
-  perl -i -pe 'chomp if eof' "$IO"
-}
-
-cat <<EOF
-Normalizer succeeded!
+# stack run normalize-phi < "$I" > "$IO" \
+#   || {
+#     cat <<EOF
+# Normalizer failed!
 
 
-* EO expression:
+# * EO expression:
 
-$(cat app.eo)
-
-
-* Phi expression:
-
-$(cat "$I")
+# $(cat app.eo)
 
 
-* Normalized Phi expression:
+# * Phi expression:
 
-$(cat "$IO")
+# $(cat "$I")
 
 
-* Diff:
+# * Error:
 
-$(diff "$I" "$IO" || true)
-EOF
+# $(cat "$IO")
+# EOF
+#     mv "$I" "$IO"
+#     exit 1
+#   }
 
-eo unphi
+# {
+#   export LC_ALL="C"
+#   perl -i -pe 'chomp if eof' "$IO"
+# }
 
-cp .eoc/unphi/app.xmir .eoc/2-optimize/app.xmir
+# cat <<EOF
+# Normalizer succeeded!
 
-eo print
 
-cp .eoc/print/app.eo app.eo
+# * EO expression:
 
-eo clean
-eo link
-eo --alone dataize app > after.txt
+# $(cat app.eo)
 
-# Check dataization with and without the normalizer
-# produces the same results
 
-if [ "$(cat before.txt)" == "$(cat after.txt)" ]; then
-    echo 'SUCCESS'
-else
-    echo 'FAILURE'
-    exit 1
-fi
+# * Phi expression:
+
+# $(cat "$I")
+
+
+# * Normalized Phi expression:
+
+# $(cat "$IO")
+
+
+# * Diff:
+
+# $(diff "$I" "$IO" || true)
+# EOF
+
+# eo unphi
+
+# cp .eoc/unphi/app.xmir .eoc/2-optimize/app.xmir
+
+# eo print
+
+# cp .eoc/print/app.eo app.eo
+
+# eo clean
+# eo link
+# eo --alone dataize app > after.txt
+
+# # Check dataization with and without the normalizer
+# # produces the same results
+
+# if [ "$(cat before.txt)" == "$(cat after.txt)" ]; then
+#     echo 'SUCCESS'
+# else
+#     echo 'FAILURE'
+#     exit 1
+# fi
