47f664c0005f94b56dbae18ff11d4411949fae49
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2024-02-19 17:24:36 +0300

    Define a wrapper to pretty print counter example

--- eo-phi-normalizer/test/confluence.hs
+++ eo-phi-normalizer/test/confluence.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
+import Data.List (intercalate)
 import Language.EO.Phi.Rules.Common (Context (Context), Rule, applyRules, intToBytes)
 import Language.EO.Phi.Rules.Yaml (convertRule, parseRuleSetFromFile, rules)
+import Language.EO.Phi.Syntax (printTree)
 import Language.EO.Phi.Syntax.Abs as Phi
 import Test.QuickCheck
 
@@ -15,11 +19,16 @@ instance Arbitrary Attribute where
       , pure VTX
       , Label . LabelId <$> listOf1 (elements ['a' .. 'z'])
       ]
+  shrink = genericShrink
 
+instance Arbitrary LabelId where
+  arbitrary = LabelId <$> arbitrary
+instance Arbitrary AlphaIndex where
+  arbitrary = AlphaIndex <$> arbitrary
 instance Arbitrary Bytes where
   arbitrary = Bytes <$> arbitrary
 instance Arbitrary Phi.Function where
-  arbitrary = Phi.Function <$> arbitrary
+  arbitrary = Phi.Function <$> listOf1 (elements ['a' .. 'z'])
 instance Arbitrary Phi.MetaId where
   arbitrary = Phi.MetaId <$> arbitrary
 instance Arbitrary Phi.MetaFunctionName where
@@ -29,9 +38,9 @@ instance Arbitrary Binding where
   arbitrary =
     oneof
       [ EmptyBinding <$> arbitrary
-      , liftA2 AlphaBinding arbitrary arbitrary
+      , AlphaBinding <$> arbitrary <*> arbitrary
       , DeltaBinding . intToBytes <$> arbitrarySizedNatural
-      , LambdaBinding . Function <$> arbitrary
+      , LambdaBinding <$> arbitrary
       ]
   shrink = genericShrink
 
@@ -46,20 +55,29 @@ instance Arbitrary Object where
           [ Formation <$> listOf arbitraryBinding
           , liftA2 Application arbitraryObj (listOf arbitraryBinding)
           , liftA2 ObjectDispatch arbitraryObj arbitraryAttr
-          , pure GlobalObject
+          , ObjectDispatch GlobalObject <$> arbitraryAttr
           , pure ThisObject
           , pure Termination
           ]
       else pure $ Formation []
   shrink = genericShrink
 
-confluence :: [Rule] -> Object -> Property
-confluence originalRules input = length (applyRules ctx input) === 1
+confluence :: [Rule] -> Object -> Bool
+confluence originalRules input = length (applyRules ctx input) == 1
  where
   ctx = Context originalRules [input]
+
+data PrettyObject = PrettyObject Context Object
+
+instance Show PrettyObject where
+  show (PrettyObject ctx obj) = intercalate "\n" (map printTree (obj : applyRules ctx obj))
 
 main :: IO ()
 main = do
   ruleset <- parseRuleSetFromFile "./test/eo/phi/rules/yegor.yaml"
   let rulesFromYaml = map convertRule (rules ruleset)
-  quickCheck (confluence rulesFromYaml)
+  let genPrettyObject = do
+        obj <- arbitrary
+        return (PrettyObject (Context rulesFromYaml [obj]) obj)
+      shrinkPrettyObject (PrettyObject ctx obj) = PrettyObject ctx <$> shrink obj
+  quickCheck $ within 10_000_000 (forAllShrink genPrettyObject shrinkPrettyObject $ \(PrettyObject _ obj) -> confluence rulesFromYaml obj)
