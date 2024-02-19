3422d86885397da6d3e306fe78bf3b4cfd6c37a3
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2024-02-19 18:08:02 +0300

    Confluence via critical pairs

--- eo-phi-normalizer/test/confluence.hs
+++ eo-phi-normalizer/test/confluence.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 import Data.List (intercalate)
-import Language.EO.Phi.Rules.Common (Context (Context), Rule, applyRules, intToBytes)
+import qualified Data.List as List
+import Language.EO.Phi.Rules.Common (Context (Context), Rule, applyRules, intToBytes, applyOneRule)
 import Language.EO.Phi.Rules.Yaml (convertRule, parseRuleSetFromFile, rules)
 import Language.EO.Phi.Syntax (printTree)
 import Language.EO.Phi.Syntax.Abs as Phi
@@ -72,6 +74,59 @@ data PrettyObject = PrettyObject Context Object
 instance Show PrettyObject where
   show (PrettyObject ctx obj) = intercalate "\n" (map printTree (obj : applyRules ctx obj))
 
+data CriticalPair = CriticalPair
+  { sourceTerm :: Object
+  , criticalPair :: (Object, Object)
+  -- add rules that were used to get the pair
+  }
+
+genCriticalPair :: [Rule] -> Gen CriticalPair
+genCriticalPair rules = do
+  (sourceTerm, results) <- fan `suchThat` \(_, rs) -> length rs > 1
+  case results of
+    x:y:_ -> return CriticalPair
+      { sourceTerm = sourceTerm
+      , criticalPair = (x, y)
+      }
+    _ -> error "IMPOSSIBLE HAPPENED"
+  where
+    fan = do
+      obj <- Formation <$> listOf arbitrary
+      return (obj, applyOneRule (Context rules [obj]) obj)
+
+shrinkCriticalPair :: [Rule] -> CriticalPair -> [CriticalPair]
+shrinkCriticalPair rules CriticalPair{..} =
+  [ CriticalPair
+      { sourceTerm = sourceTerm'
+      , criticalPair = (x, y)
+      }
+  | sourceTerm'@Formation{} <- shrink sourceTerm
+  , x:y:_ <- [applyOneRule (Context rules [sourceTerm']) sourceTerm']
+  ]
+
+descendantsN :: Int -> [Rule] -> [Object] -> [Object]
+descendantsN maxDepth rules objs
+  | maxDepth <= 0 = objs
+  | otherwise = objs ++ descendantsN (maxDepth - 1) rules
+      [ obj'
+      | obj <- objs
+      , obj' <- applyOneRule (Context rules [obj]) obj ]
+
+confluentCriticalPairN :: Int -> [Rule] -> CriticalPair -> Bool
+confluentCriticalPairN maxDepth rules CriticalPair{..} =
+  not (null (descendantsN maxDepth rules [x] `List.intersect` descendantsN maxDepth rules [y]))
+  where
+    (x, y) = criticalPair
+
+instance Show CriticalPair where
+  show CriticalPair{criticalPair = (x, y), ..} = intercalate "\n"
+    [ "Source term:"
+    , "  " <> printTree sourceTerm
+    , "Critical pair:"
+    , "  " <> printTree x
+    , "  " <> printTree y
+    ]
+
 main :: IO ()
 main = do
   ruleset <- parseRuleSetFromFile "./test/eo/phi/rules/yegor.yaml"
@@ -80,4 +135,8 @@ main = do
         obj <- arbitrary
         return (PrettyObject (Context rulesFromYaml [obj]) obj)
       shrinkPrettyObject (PrettyObject ctx obj) = PrettyObject ctx <$> shrink obj
-  quickCheck $ within 10_000_000 (forAllShrink genPrettyObject shrinkPrettyObject $ \(PrettyObject _ obj) -> confluence rulesFromYaml obj)
+  quickCheck $
+    within 100_000 $
+      forAllShrink (genCriticalPair rulesFromYaml) (shrinkCriticalPair rulesFromYaml) $
+        confluentCriticalPairN 10 rulesFromYaml
+  -- quickCheck $ within 10_000_000 (forAllShrink genPrettyObject shrinkPrettyObject $ \(PrettyObject _ obj) -> confluence rulesFromYaml obj)
