e70b88a88c6068933ad2614654fd9f0aa98fbbf0
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-03-21 19:55:26 +0300

    fix(cli):
    - create directory for report html if missing
    
    - fix field names

--- eo-phi-normalizer/app/Main.hs
+++ eo-phi-normalizer/app/Main.hs
@@ -41,7 +41,7 @@ import GHC.Generics (Generic)
 import Language.EO.Phi (Bytes (Bytes), Object (Formation), Program (Program), parseProgram, printTree)
 import Language.EO.Phi.Dataize (dataizeRecursively, dataizeRecursivelyChain, dataizeStep, dataizeStepChain)
 import Language.EO.Phi.Metrics as Metrics (ProgramMetrics (..), getProgramMetrics, splitPath)
-import Language.EO.Phi.Report.Data as Report (ReportConfig (..), ReportItem (..), makeProgramReport, makeReport)
+import Language.EO.Phi.Report.Data as Report (ReportConfig (..), ReportItem (..), ReportPage (..), makeProgramReport, makeReport)
 import Language.EO.Phi.Report.Html as Report (toHtmlReport)
 import Language.EO.Phi.Rules.Common (ApplicationLimits (ApplicationLimits), applyRulesChainWith, applyRulesWith, defaultContext, objectSize, propagateName1)
 import Language.EO.Phi.Rules.Yaml (RuleSet (rules, title), convertRuleNamed, parseRuleSetFromFile)
@@ -421,6 +421,9 @@ main = do
         metricsPhi <- getMetrics item.bindingsPathPhi (Just item.phi)
         metricsPhiNormalized <- getMetrics item.bindingsPathPhiNormalized (Just item.phiNormalized)
         pure $ makeProgramReport item metricsPhi metricsPhiNormalized
-      let reportHtml = toHtmlReport reportConfig (makeReport programReports)
-      createDirectoryIfMissing True (takeDirectory reportConfig.reportHtml)
-      writeFile (reportConfig.reportDirectory </> reportConfig.reportHtml) (unpack $ renderMarkup reportHtml)
+      let report = makeReport programReports
+          reportHtml = toHtmlReport reportConfig report
+          pageHtmlPath = reportConfig.reportPage.directory </> reportConfig.reportPage.html
+
+      createDirectoryIfMissing True (takeDirectory pageHtmlPath)
+      writeFile (reportConfig.reportPage.directory </> reportConfig.reportPage.html) (unpack $ renderMarkup reportHtml)
