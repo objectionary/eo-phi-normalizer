6e6bc669cf90f37c45f217f93b6b64abaa628056
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-03-22 13:48:06 +0300

    refactor(report):
    - use safe numbers
    
    - use change categories in the report

--- eo-phi-normalizer/src/Language/EO/Phi/Report/Data.hs
+++ eo-phi-normalizer/src/Language/EO/Phi/Report/Data.hs
@@ -11,11 +11,12 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -Wno-partial-fields #-}
 
 module Language.EO.Phi.Report.Data where
 
 import GHC.Generics (Generic)
-import Language.EO.Phi.Metrics (BindingMetrics (..), Metrics (..), MetricsCount, ProgramMetrics, makeUnaryOperation)
+import Language.EO.Phi.Metrics (BindingMetrics (..), Metrics (..), MetricsCount, ProgramMetrics, SafeNumber (..))
 import Language.EO.Phi.Metrics qualified as Metrics
 import Language.EO.Phi.TH (deriveJSON)
 import Prelude hiding (div, id, span)
@@ -30,7 +31,21 @@ data ReportItem = ReportItem
 
 $(deriveJSON ''ReportItem)
 
-type MetricsChange = Metrics Double
+data MetricsChangeCategory a
+  = MetricsChange'Good {change :: a}
+  | MetricsChange'Bad {change :: a}
+  | MetricsChange'NaN
+  deriving stock (Show, Generic, Eq)
+
+$(deriveJSON ''MetricsChangeCategory)
+
+type MetricsChange = Metrics (SafeNumber Double)
+
+newtype Percent = Percent {percent :: Double}
+
+$(deriveJSON ''Percent)
+
+type MetricsChangeCategorized = Metrics (MetricsChangeCategory Percent)
 
 data ReportPage = ReportPage
   { directory :: FilePath
@@ -59,7 +74,7 @@ data ReportRow = ReportRow
   , bindingsPathAfter :: Maybe Metrics.Path
   , attributeBefore :: Maybe String
   , attributeAfter :: Maybe String
-  , metricsChange :: MetricsChange
+  , metricsChange :: MetricsChangeCategorized
   , metricsBefore :: Metrics Int
   , metricsAfter :: Metrics Int
   }
@@ -80,15 +95,21 @@ data Report = Report
 
 $(deriveJSON ''Report)
 
-calculateMetricsChange :: MetricsCount -> MetricsCount -> MetricsChange
-calculateMetricsChange before after =
-  (before' - after') / before'
+calculateMetricsChange :: MetricsChange -> MetricsCount -> MetricsCount -> MetricsChangeCategorized
+calculateMetricsChange expectedMetricsChange before after =
+  getMetricsChangeClassified <$> expectedMetricsChange <*> actualMetricsChange
  where
-  before' = makeUnaryOperation fromIntegral before
-  after' = makeUnaryOperation fromIntegral after
+  getMetricsChangeClassified (SafeNumber'Number expected) (SafeNumber'Number actual)
+    | actual >= expected = MetricsChange'Good (Percent actual)
+    | otherwise = MetricsChange'Bad (Percent actual)
+  getMetricsChangeClassified _ _ = MetricsChange'NaN
+  actualMetricsChange :: MetricsChange
+  actualMetricsChange = (before' - after') / before'
+  before' = fromIntegral <$> before
+  after' = fromIntegral <$> after
 
-makeProgramReport :: ReportItem -> ProgramMetrics -> ProgramMetrics -> ProgramReport
-makeProgramReport reportItem metricsPhi metricsPhiNormalized =
+makeProgramReport :: ReportConfig -> ReportItem -> ProgramMetrics -> ProgramMetrics -> ProgramReport
+makeProgramReport reportConfig reportItem metricsPhi metricsPhiNormalized =
   ProgramReport{..}
  where
   bindingsRows =
@@ -101,7 +122,7 @@ makeProgramReport reportItem metricsPhi metricsPhiNormalized =
           , bindingsPathAfter = Just bindingsMetricsAfter.path
           , attributeBefore = Just attributeBefore
           , attributeAfter = Just attributeAfter
-          , metricsChange = calculateMetricsChange metricsBefore metricsAfter
+          , metricsChange = calculateMetricsChange reportConfig.expectedMetricsChange metricsBefore metricsAfter
           , metricsBefore = metricsBefore
           , metricsAfter = metricsAfter
           }
@@ -117,19 +138,19 @@ makeProgramReport reportItem metricsPhi metricsPhiNormalized =
       , bindingsPathAfter = Nothing
       , attributeBefore = Nothing
       , attributeAfter = Nothing
-      , metricsChange = calculateMetricsChange metricsPhi.programMetrics metricsPhiNormalized.programMetrics
+      , metricsChange = calculateMetricsChange reportConfig.expectedMetricsChange metricsPhi.programMetrics metricsPhiNormalized.programMetrics
       , metricsBefore = metricsPhi.programMetrics
       , metricsAfter = metricsPhiNormalized.programMetrics
       }
 
-makeReport :: [ProgramReport] -> Report
-makeReport programReports =
+makeReport :: ReportConfig -> [ProgramReport] -> Report
+makeReport reportConfig programReports =
   Report{..}
  where
   programRows = (.programRow) <$> programReports
   metricsBefore = foldMap (.metricsBefore) programRows
   metricsAfter = foldMap (.metricsAfter) programRows
-  metricsChange = calculateMetricsChange metricsBefore metricsAfter
+  metricsChange = calculateMetricsChange reportConfig.expectedMetricsChange metricsBefore metricsAfter
   totalRow =
     ReportRow
       { fileBefore = Nothing
