e24d103f208716e91b98d497b943b2f70c16a348
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-03-23 22:47:41 +0300

    fix(test): formatting

--- eo-phi-normalizer/test/Language/EO/PhiSpec.hs
+++ eo-phi-normalizer/test/Language/EO/PhiSpec.hs
@@ -16,8 +16,8 @@ import Data.String (IsString (..))
 import Data.String.Interpolate (i)
 import Data.Yaml (decodeFileThrow)
 import Language.EO.Phi
-import Language.EO.Phi.Metrics.Data (BindingsByPathMetrics (..), ProgramMetrics (..))
 import Language.EO.Phi.Metrics.Collect (getProgramMetrics)
+import Language.EO.Phi.Metrics.Data (BindingsByPathMetrics (..), ProgramMetrics (..))
 import Language.EO.Phi.Rules.Common (Rule, defaultContext, equalProgram)
 import Language.EO.Phi.Rules.PhiPaper (rule1, rule6)
 import Test.EO.Phi
