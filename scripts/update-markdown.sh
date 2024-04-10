7b6ec409e41a2cc364493b42728fbd9b4e962867
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-04-10 14:51:20 +0300

    fix(scripts): directories in update-markdown

--- scripts/update-markdown.sh
+++ scripts/update-markdown.sh
@@ -2,15 +2,14 @@ mdsh
 
 mdsh -i site/docs/src/common/sample-program.md --work_dir .
 mdsh -i site/docs/src/common/celsius.md --work_dir .
-mdsh -i site/docs/src/commands/normalizer.md --work_dir .
-mdsh -i site/docs/src/commands/normalizer-transform.md --work_dir .
-mdsh -i site/docs/src/commands/normalizer-metrics.md --work_dir .
-mdsh -i site/docs/src/commands/normalizer-dataize.md --work_dir .
-mdsh -i site/docs/src/commands/normalizer-report.md --work_dir .
+mdsh -i site/docs/src/normalizer.md --work_dir .
+mdsh -i site/docs/src/normalizer/transform.md --work_dir .
+mdsh -i site/docs/src/normalizer/metrics.md --work_dir .
+mdsh -i site/docs/src/normalizer/dataize.md --work_dir .
+mdsh -i site/docs/src/normalizer/report.md --work_dir .
 mdsh -i site/docs/src/contributing.md --work_dir .
 
 rm program.phi celsius.phi
 
 npm i
 npx prettier -w "**/*.md"
-
