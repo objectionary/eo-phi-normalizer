f80da8b45de7d284de21202b5c9f31288b41490e
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-04-29 09:35:36 +0300

    refactor(normalizer): use OverloadedStrings in more places

--- eo-phi-normalizer/src/Language/EO/Phi/ToLaTeX.hs
+++ eo-phi-normalizer/src/Language/EO/Phi/ToLaTeX.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Language.EO.Phi.ToLaTeX where
 
 import Data.Foldable (fold)
 import Data.List (intersperse)
+import Data.String (IsString)
 import Data.Text qualified as T
 import Language.EO.Phi (AlphaIndex (..), Attribute (..), Binding (..), Bytes (..), Function (..), LabelId (..), Object (..), Program (Program))
 import Text.Regex (mkRegex, subRegex)
 
 newtype LaTeX = LaTeX String
-  deriving (Show)
+  deriving newtype (Show, IsString)
 
 instance Semigroup LaTeX where
   (LaTeX x) <> (LaTeX y) = LaTeX (x <> y)
@@ -23,41 +26,36 @@ class ToLatex a where
 
 instance ToLatex Program where
   toLatex (Program bindings) =
-    LaTeX "\\Big\\{ " <> toLatex (Formation bindings) <> LaTeX " \\Big\\}"
+    "\\Big\\{ " <> toLatex (Formation bindings) <> " \\Big\\}"
 
 instance ToLatex Attribute where
-  toLatex Phi = LaTeX "@"
-  toLatex Rho = LaTeX "^"
-  toLatex Sigma = LaTeX "&"
+  toLatex Phi = "@"
+  toLatex Rho = "^"
+  toLatex Sigma = "&"
   toLatex (Alpha (AlphaIndex a)) = LaTeX ("\\alpha_" ++ tail a)
   toLatex (Label (LabelId l)) = LaTeX l
   toLatex (MetaAttr _) = error "rendering MetaBindings in LaTex format"
   toLatex VTX = error "rendering VTX in LaTex format"
 
 instance ToLatex Binding where
-  toLatex (AlphaBinding attr obj) =
-    toLatex attr <> LaTeX " -> " <> toLatex obj
-  toLatex (EmptyBinding attr) =
-    toLatex attr <> LaTeX " -> ?"
-  toLatex (DeltaBinding (Bytes bytes)) =
-    LaTeX "D> " <> LaTeX bytes
-  toLatex DeltaEmptyBinding =
-    LaTeX "D> ?"
-  toLatex (LambdaBinding (Function fn)) =
-    LaTeX "L> " <> LaTeX fn
+  toLatex (AlphaBinding attr obj) = toLatex attr <> " -> " <> toLatex obj
+  toLatex (EmptyBinding attr) = toLatex attr <> " -> ?"
+  toLatex (DeltaBinding (Bytes bytes)) = "D> " <> LaTeX bytes
+  toLatex DeltaEmptyBinding = "D> ?"
+  toLatex (LambdaBinding (Function fn)) = "L> " <> LaTeX fn
   toLatex (MetaBindings _) = error "rendering MetaBindings in LaTex format"
   toLatex (MetaDeltaBinding _) = error "rendering MetaDeltaBinding in LaTex format"
 
 instance ToLatex Object where
   toLatex (Formation bindings) =
-    LaTeX "[[ " <> fold (intersperse (LaTeX ", ") (map toLatex bindings)) <> LaTeX " ]]"
+    "[[ " <> fold (intersperse ", " (map toLatex bindings)) <> " ]]"
   toLatex (Application obj bindings) =
-    toLatex obj <> LaTeX "( " <> fold (intersperse (LaTeX ", ") (map toLatex bindings)) <> LaTeX " )"
+    toLatex obj <> "( " <> fold (intersperse ", " (map toLatex bindings)) <> " )"
   toLatex (ObjectDispatch obj attr) =
-    toLatex obj <> LaTeX "." <> toLatex attr
-  toLatex GlobalObject = LaTeX "Q"
-  toLatex ThisObject = LaTeX "$"
-  toLatex Termination = LaTeX "\\dead"
+    toLatex obj <> "." <> toLatex attr
+  toLatex GlobalObject = "Q"
+  toLatex ThisObject = "$"
+  toLatex Termination = "\\dead"
   toLatex (MetaObject _) = error "rendering MetaObject in LaTex format"
   toLatex (MetaFunction _ _) = error "rendering MetaFunction in LaTex format"
 
