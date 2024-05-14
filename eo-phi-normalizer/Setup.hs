6c4a71d1ab11238a7d6a11df8a45e1b558e4f157
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-05-14 09:57:25 +0300

    fix(normalizer): in Setup.hs - generate Syntax files every time - check that the command succeeds - set UTF8 when running on Windows - run a single-line command (no idea why next commands don't run after chcp)

--- eo-phi-normalizer/Setup.hs
+++ eo-phi-normalizer/Setup.hs
@@ -1,36 +1,49 @@
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 -- Source: https://github.com/haskell/cabal/issues/6726#issuecomment-918663262
 
 -- | Custom Setup that runs bnfc to generate the language sub-libraries
 -- for the parsers included in Ogma.
 module Main (main) where
 
+import Data.List (intercalate)
 import Distribution.Simple (defaultMainWithHooks, hookedPrograms, postConf, preBuild, simpleUserHooks)
 import Distribution.Simple.Program (Program (..), findProgramVersion, simpleProgram)
+import System.Exit (ExitCode (..))
 import System.Process (system)
-import PyF (fmt)
 
 -- | Run BNFC, happy, and alex on the grammar before the actual build step.
 --
 -- All options for bnfc are hard-coded here.
 main :: IO ()
-main = defaultMainWithHooks $ simpleUserHooks
-  { hookedPrograms = [ bnfcProgram ]
-  , postConf       = \args flags packageDesc localBuildInfo -> do
-#ifndef mingw32_HOST_OS
-      _ <- system [fmt|
-            bnfc --haskell -d -p Language.EO.Phi --generic -o src/ grammar/EO/Phi/Syntax.cf
-            cd src/Language/EO/Phi/Syntax
-            alex Lex.x
-            happy Par.y
-          |]
+main =
+  defaultMainWithHooks $
+    simpleUserHooks
+      { hookedPrograms = [bnfcProgram]
+      , postConf = \args flags packageDesc localBuildInfo -> do
+          let
+            isWindows =
+#ifdef mingw32_HOST_OS
+                      True
+#else
+                      False
 #endif
-      postConf simpleUserHooks args flags packageDesc localBuildInfo
-  }
+            command = intercalate " && " $
+              ["chcp 65001" | isWindows] <> [
+                "bnfc --haskell -d -p Language.EO.Phi --generic -o src/ grammar/EO/Phi/Syntax.cf",
+                "cd src/Language/EO/Phi/Syntax",
+                "alex Lex.x",
+                "happy Par.y"
+              ]
+          putStrLn command
+          ExitSuccess <- system command
+          postConf simpleUserHooks args flags packageDesc localBuildInfo
+      }
 
 -- | NOTE: This should be in Cabal.Distribution.Simple.Program.Builtin.
 bnfcProgram :: Program
-bnfcProgram = (simpleProgram "bnfc")
-  { programFindVersion = findProgramVersion "--version" id
-  }
+bnfcProgram =
+  (simpleProgram "bnfc")
+    { programFindVersion = findProgramVersion "--version" id
+    }
