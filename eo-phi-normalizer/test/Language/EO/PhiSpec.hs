246aafa176fc69c11958cc99908f36bedc26a948
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-12-30 01:17:33 +0300

    refactor(eo-phi-normalizer): remove unused code, use explicit imports

--- eo-phi-normalizer/test/Language/EO/PhiSpec.hs
+++ eo-phi-normalizer/test/Language/EO/PhiSpec.hs
@@ -25,10 +25,8 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedRecordDot #-}
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
 
 module Language.EO.PhiSpec where
 
@@ -37,15 +35,12 @@ import Data.Char (isSpace)
 import Data.List (dropWhileEnd)
 import Data.String (IsString (..))
 import Data.Yaml (decodeFileThrow)
-import Language.EO.Phi
-import Language.EO.Phi.Dataize.Context (defaultContext)
+import Language.EO.Phi (Binding (..), Object, Program (..), normalize, printTree)
 import Language.EO.Phi.Metrics.Collect (getProgramMetrics)
 import Language.EO.Phi.Metrics.Data (BindingsByPathMetrics (..), ProgramMetrics (..))
-import Language.EO.Phi.Rules.Common (Rule, equalProgram)
-import Language.EO.Phi.Rules.PhiPaper (rule6)
-import PyF (fmt)
-import Test.EO.Phi
-import Test.Hspec
+import Language.EO.Phi.Rules.Common (equalProgram)
+import Test.EO.Phi (PhiTest (..), PhiTestGroup (..), allPhiTests)
+import Test.Hspec (Spec, describe, it, runIO, shouldBe, shouldSatisfy)
 import Test.Metrics.Phi (MetricsTest (..), MetricsTestSet (..))
 
 applyRule :: (Object -> [Object]) -> Program -> [Program]
