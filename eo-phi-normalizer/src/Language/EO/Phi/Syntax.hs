cb7045687ed878e14ea3b87ddcee4bee6485a713
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-01-30 02:28:41 +0300

    fix(eo-phi-normalizer): add pragma

--- eo-phi-normalizer/src/Language/EO/Phi/Syntax.hs
+++ eo-phi-normalizer/src/Language/EO/Phi/Syntax.hs
@@ -21,6 +21,7 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 {- FOURMOLU_ENABLE -}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE LambdaCase #-}
